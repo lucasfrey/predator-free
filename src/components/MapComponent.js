e93686fd8e5f8c13e65b5f9afbc21ab475cc5c8e
Lucas Frey <lucas.frey@heyday.co.nz>
2019-05-12 21:12:41 +1200

    Add basic filters and some deployed traps

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import { withScriptjs, withGoogleMap, GoogleMap, Marker, KmlLayer } from "react-google-maps"
 
-export default withScriptjs(withGoogleMap((props) =>
-  <GoogleMap
-    defaultZoom={14}
-    defaultCenter={{ lat: -41.31149330989544, lng: 174.77141979509724 }}
-  >
-	<KmlLayer
-      url="http://www.google.com/maps/d/u/0/kml?mid=1R8Rw7tZTGw3YHKBTNBOh-LKU5sw"
-      options={{ preserveViewport: true }}
-    />
-    {props.isMarkerShown && <Marker position={{ lat: -41.3090813, lng: 174.76115800000002 }} />}
-  </GoogleMap>
-))
+class MapComponent extends React.Component {
+	render() {
+		return (
+			<GoogleMap
+				defaultZoom={14}
+				defaultCenter={{ lat: -41.31149330989544, lng: 174.77141979509724 }}>
+				{this.props.showZones && <KmlLayer
+					url="https://raw.githubusercontent.com/lucasfrey/predator-free/master/static/data/Team-Leader-Zones.kml"
+					options={{ preserveViewport: true }}
+				/>}
+
+				{this.props.showZones && <KmlLayer
+					url="https://raw.githubusercontent.com/lucasfrey/predator-free/master/static/data/Deployed-Traps-21-Aug-18.kml"
+					options={{ preserveViewport: true }}
+				/>}
+				
+				{this.props.isMarkerShown && <Marker position={{ lat: -41.3090813, lng: 174.76115800000002 }} />}
+			</GoogleMap>
+		)
+	}
+}
+
+export default withScriptjs(withGoogleMap(MapComponent))
--- src/pages/map.js
+++ src/pages/map.js
@@ -7,28 +7,60 @@ import Layout from "../components/layout"
 import MapComponent from "../components/MapComponent"
 
 class MapIndex extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			showZones: true,
+			showMarkers: true,
+			showTraps: true
+		}
+	}
+
 	render() {
 		const siteTitle = get(this, 'props.data.site.siteMetadata.title')
 		
 		return (
-		<Layout location={this.props.location} >
-			<div style={{ background: '#fff' }}>
-				<Helmet title={siteTitle} />
-				<div className={styles.hero}>
-					<h4>Map</h4>
+			<Layout location={this.props.location} >
+				<div style={{ background: '#fff' }}>
+					<Helmet title={siteTitle} />
+					<div className={styles.hero}>
+						<h4>Map</h4>
+					</div>
+					<div className="wrapper">
+						{this.renderFilters()}
+						<MapComponent 
+							isMarkerShown={this.state.showMarkers}
+							showZones={this.state.showZones}
+							googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAV9GarEbjNh5ryLf-AMsk0WAMIbwFmrYE&libraries=geometry,drawing,places"
+							loadingElement={<div style={{ height: `100%` }} />}
+							containerElement={<div style={{ height: `600px` }} />}
+							mapElement={<div style={{ height: `100%` }} />}
+						/>
+					</div>
 				</div>
-				<div className="wrapper">
-					<MapComponent 
-						isMarkerShown 
-						googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAV9GarEbjNh5ryLf-AMsk0WAMIbwFmrYE&libraries=geometry,drawing,places"
-						loadingElement={<div style={{ height: `100%` }} />}
-						containerElement={<div style={{ height: `400px` }} />}
-						mapElement={<div style={{ height: `100%` }} />}
-					  />
-				</div>
-			</div>
-		</Layout>
+			</Layout>
 		)
+	}
+
+	renderFilters() {
+		return (
+			<div>
+				<h4>Filters</h4>
+				<input type="checkbox" checked={this.state.showZones} id="showZones" name="showZones" onChange={this.handleChange} />
+				<label htmlFor="showZones">Show zones</label><br />
+				<input type="checkbox" checked={this.state.showMarkers} id="showMarkers" name="showMarkers" onChange={this.handleChange} />
+				<label htmlFor="showMarkers">Show markers</label><br/>
+				<input type="checkbox" checked={this.state.showTraps} id="showTraps" name="showTraps" onChange={this.handleChange} />
+				<label htmlFor="showTraps">Show traps</label><br/>
+			</div>
+		)
+	}
+
+	handleChange = (e) => {
+		this.setState({
+			[e.target.name]: !this.state[e.target.name]
+		})
 	}
 }
 
