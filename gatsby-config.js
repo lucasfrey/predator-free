1f90aab2618423f032c4f6cdbe58b585e2fd4439
Lucas Frey <lucas.frey@heyday.co.nz>
2019-06-11 20:51:40 +1200

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
         background_color: "#6b37bf",
         theme_color: "#6b37bf",
         display: "standalone",
-        icon: "src/images/favicon.ico",
+        icon: "src/images/favicon.png",
         crossOrigin: `use-credentials`,
       }
     },
