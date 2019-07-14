cf5e54553c932a59bb82e29e93d6510f800033a4
Lucas Frey <lucas.frey@heyday.co.nz>
2019-07-14 21:58:58 +1200

    Implement some snazzy map styles, can't look at the default style anymore

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -12,10 +12,14 @@ class MapComponent extends React.Component {
 	};
 
 	render() {
+		const { props } = this;
+		const { mapStyles } = props;
+
 		return (
 			<GoogleMap
 				defaultZoom={14}
-				defaultCenter={{ lat: -41.31149330989544, lng: 174.77141979509724 }}>
+				defaultCenter={{ lat: -41.31149330989544, lng: 174.77141979509724 }}
+				defaultOptions={{ styles: mapStyles }}>
 				{this.props.showZones && <KmlLayer
 					url="https://raw.githubusercontent.com/lucasfrey/predator-free/master/static/data/Team-Leader-Zones.kml"
 					options={{ preserveViewport: true }}
@@ -42,6 +46,11 @@ class MapComponent extends React.Component {
 		}
 	
 	}
+}
+
+MapComponent.defaultProps = {
+	// The style is copy from https://snazzymaps.com/style/2/midnight-commander
+	mapStyles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
 }
 
 export default withScriptjs(withGoogleMap(MapComponent))
